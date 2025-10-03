021d2466fc0ef225414ac2082bed5f9742650ec9
ramchandra2011 <ramchandraz2011@gmail.com>
2025-10-03 15:23:47 +0530

    updated web page

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Initialize Animate On Scroll (AOS)
+document.addEventListener('DOMContentLoaded', function() {
+    AOS.init({
+        duration: 800, // values from 0 to 3000, with step 50ms
+        once: true, // whether animation should happen only once - while scrolling down
+    });
+});
